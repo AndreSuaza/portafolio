4b3c63e4e44714380f33ce5b541e9a34db6d5a25
Andre Suaza <suaza.andre@gmail.com>
2017-11-14 17:31:43 -0500

    pagina terminada

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,12 +1,12 @@
 import { RouterModule, Routes } from '@angular/router';
-import { AboutComponent, PortafolioComponent, ItemComponent } from "./components/index.paginas";
+import { AboutComponent, PortafolioComponent, ItemComponent, SearchComponent } from "./components/index.paginas";
 
 
 const app_routes: Routes = [
   { path: 'home', component: PortafolioComponent },
   { path: 'about', component: AboutComponent },
   { path: 'producto/:id', component: ItemComponent }
-  { path: 'buscar/:termino', component: ItemComponent },
+  { path: 'buscar/:termino', component: SearchComponent },
   { path: '**', pathMatch: 'full', redirectTo: 'home' }
 ];
 
--- src/app/services/productos.service.ts
+++ src/app/services/productos.service.ts
@@ -5,10 +5,42 @@ import { Http } from '@angular/http';
 export class ProductosService {
 
   productos:any[] = [];
+  productos_filtrado:any[] = [];
   cargando:boolean = true;
 
   constructor( public http:Http ) {
     this.cargar_productos();
+  }
+
+  public buscar_producto(termino:String){
+
+    if(this.productos.length === 0){
+      this.cargar_productos().then( ()=>{
+        //termino carga
+        this.filtrar_productos(termino);
+      });
+    }else{
+      this.filtrar_productos(termino);
+    }
+
+
+  }
+
+  private filtrar_productos(termino:string){
+
+    this.productos_filtrado = [];
+
+    termino = termino.toLowerCase();
+
+    this.productos.forEach( prod => {
+
+      if( prod.categoria.indexOf(termino) >= 0 || prod.titulo.toLowerCase().indexOf(termino) >= 0){
+        this.productos_filtrado.push(prod);
+        console.log( prod );
+      }
+
+      //console.log( prod );
+    })
   }
 
   public cargar_producto( id:string ){
@@ -19,12 +51,20 @@ export class ProductosService {
 
   public cargar_productos(){
 
-      this.http.get('https://paginaweb-818a7.firebaseio.com/productos_idx.json')
-        .subscribe( res=> {
-          //console.log(res.json();)
-          this.cargando = false;
-          this.productos = res.json();
-        })
+      let promesa = new Promise((resolve,reject)=>{
+
+        this.http.get('https://paginaweb-818a7.firebaseio.com/productos_idx.json')
+          .subscribe( res=> {
+            //console.log(res.json();)
+            this.cargando = false;
+            this.productos = res.json();
+            resolve();
+          });
+
+      });
+
+      return promesa;
+
   }
 
 }
